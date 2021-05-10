b00631b2316a5b8e15b7311ec75bf2d4329c9fef
indiangravy <60591283+indiangravy@users.noreply.github.com>
2021-05-10 10:53:49 -0700

    Initial commit

---
+++ src/clientside/ClientSide.java
@@ -0,0 +1,29 @@
+package clientside;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientSide {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            return;
+        }
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        try {
+            Socket server = new Socket(host, port);
+            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Send Message >>>  ");
+            String message = sc.nextLine();
+            out.println(message);
+            System.out.println(in.readLine());
+        } catch (Exception e) {}
+    }
+    
+}
