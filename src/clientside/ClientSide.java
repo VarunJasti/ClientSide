d38ff387a1694838ab810d607b82b05c97645667
indiangravy <60591283+indiangravy@users.noreply.github.com>
2021-05-11 07:56:05 -0700

    Update ClientSide.java

--- src/clientside/ClientSide.java
+++ src/clientside/ClientSide.java
@@ -1,21 +1,20 @@
 package clientside;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientSide {
+    
+    final static String HOST = "104.209.44.90";
+    final static int PORT = 4444;
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            return;
-        }
-        String host = args[0];
-        int port = Integer.parseInt(args[1]);
         try {
-            Socket server = new Socket(host, port);
+            Socket server = new Socket(HOST, PORT);
             BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
             PrintWriter out = new PrintWriter(server.getOutputStream(), true);
             Scanner sc = new Scanner(System.in);
@@ -23,7 +22,9 @@ public class ClientSide {
             String message = sc.nextLine();
             out.println(message);
             System.out.println(in.readLine());
-        } catch (Exception e) {}
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
     
 }
